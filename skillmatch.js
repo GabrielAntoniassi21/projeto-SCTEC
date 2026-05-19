4213a9a22e444fe27f75f220170d7bd56ed3f95c
GabrielAntoniassi21 <gabriel.antoniassi21@gmail.com>
2026-05-18 21:20:41 -0300

    classe vaga, candidato, objeto candidato, array vagas

---
+++ skillmatch.js
@@ -0,0 +1,60 @@
+class candidato {
+    constructor(nome, area, habilidades, experienciaMeses) {
+        this.nome = nome;
+        this.area = area;
+        this.habilidades = habilidades;
+        this.experienciaMeses = experienciaMeses;
+    }
+}
+
+class Vaga {
+    constructor(id, empresa, cargo, requisitos, salario) {
+        this.id = id;
+        this.empresa = empresa;
+        this.cargo = cargo;
+        this.requisitos = requisitos;
+        this.salario = salario;
+    }
+}
+
+class VagaFrontEndJunior extends Vaga {
+    constructor(id, empresa, cargo, requisitos, salario, modalidade) {
+        super(id, empresa, cargo, requisitos, salario);
+        this.modalidade = modalidade;
+    }
+}
+
+const candidato = {
+    nome: "João Silva",
+    area: "Front-End",
+    habilidades: ["JavaScroipt", "GitHub", "Logica de Programação", "KanBan"],
+    experienciaMeses: 3
+};
+
+vagas = [
+    {
+        id: 1,
+        empresa: "Tech Start",
+        cargo: "Desenvolvedor Front-End Junior",
+        requisitos: ["JavaScript", "GitHub", "Logica de Programação"],
+        salario: 2800,
+        modalidade: "Remoto"
+    },
+    {
+        id: 2,
+        empresa: "CodeLab",
+        cargo: "Estagiário Front-End",
+        requisitos: ["JavaScript", "Kanban", "GitHub"],
+        salario: 1800,
+        modalidade: "Hibrido"
+    },
+    {
+        id: 3,
+        empresa: "WebSolutions",
+        cargo: "Programador JavaScript Junior",
+        requisitos: ["JavaScript", "Arrays", "Objetos", "Funções"],
+        salario: 3000,
+        modalidade: "Presencial"}
+
+];
+
